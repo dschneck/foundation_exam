51928f56b01d32558a20b8180ad58d00345ca36f
David Schneck <david.schneckster@hotmail.com>
2020-12-16 23:49:15 -0500

    Implemented tree traversals and learned some mathematical facts about complete binary trees

--- data-structures/binary-trees/tree.c
+++ data-structures/binary-trees/tree.c
@@ -1,4 +1,14 @@
-/* This is a binary tree */
+/* Binary Trees 
+
+	A complete binary tree is one where every level (except for the last) is completely filled,
+	and the nodes are as far left as possible.
+
+	Mathematical facts about a complete binary tree:
+		-> n = 2 ^ (height + 1) - 1
+		-> height = log ((n+1)/2)
+		-> has between 1 and 2 ^ h nodes at the last level
+		-> the number of internal nodes is n/2
+*/
 
 // Pre-processor directives and libraries
 #include <stdio.h>
@@ -14,11 +24,16 @@ struct node {
 
 // Function declaration
 node * newNode(int val);
-void printTree(node * root);
+
+// All of the following print methods are 
+// examples of depth first traversals
+void preorderPrint(node * root);
+void inorderPrint(node * root);
+void postorderPrint(node * root);
 
 // Main method
 int main() {
-	node * root;
+	node * root = newNode(1);
 
 	return 0;
 }
@@ -38,6 +53,45 @@ node * newNode(int val) {
 
 }
 
-void printTree(node * root) {
+void preorderPrint(node * root) {
+	// Base case 
+	if (root == NULL) return;
+
+	
+	 printf("%d\n", root->val);
+
+	// Recursive calls
+	preorderPrint(root->l);
+	preorderPrint(root->r);
+}
+
+void postorderPrint(node * root) {
+	// Base cae
+	if (root == NULL) return;
+
+	postorderPrint(root->l);
+	postorderPrint(root->r);
+
+	printf("%d\n", root->val);
+}
+
+void inorderPrint(node * root) {
+	// Base case
+	if (root == NULL) return;
+
+	inorderPrint(root->l);
+	printf("%d\n", root->val);
+	inorderPrint(root->r);
+
+
 
 }
+
+
+
+
+
+
+
+
+
