664ee425a21c73023aa620b7d3c32077fb3f6af9
David Schneck <david.schneckster@hotmail.com>
2021-01-02 18:30:34 -0500

    Working on a hash table and made another attempt at bubble sort from memory.

---
+++ data-structures/hash-tables/table.c
@@ -0,0 +1,76 @@
+/* Hash Tables
+	A hash table uses a hash function to populate an
+	array in a uniform and random way. A value is inputed into the function
+	and it returns a index in the array.
+
+	A full hash table is one that only has one empty index
+
+	Collision resolution policy - the method used to deal with the same 
+	index given to different input values. 
+	Examples:
+		- Linear probing: checking contiguous indexes to find an empty one
+		- Quadratic probing: checking indexes with squares (1, 4, 9, 16, ...) to find an empty one
+		- Double hasing: use to different hashing functions
+
+
+*/
+
+// Pre-processor directives
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 10 // Size of array
+#define HASH(x) (x % N) // Hash function
+
+#define LINEAR_PROBING
+#define QUAD_PROBING
+#define DOUB_HASH
+
+// Function delcarations
+void printTable(int * arr, int size);
+void insert(int val);
+
+int main() {
+	int * table = calloc(N, sizeof(int));
+	int tmp;
+
+	printf("This will take in %d values (only positive integers).\n", N);
+	for (int i = 0; i < N-1; i++) {
+		printf("Input value: ");
+		scanf("%d", &tmp);
+		
+		insert(tmp);
+
+		printf("\n");
+		printTable(table, N);
+	}
+
+	printTable(table, N);
+
+	free(table);
+	return 0;
+}
+
+// Function definitons
+void printTable(int * arr, int size) {
+	for (int i = 0; i < size; i++) {
+		printf("|%d| ", arr[i]);
+	}
+
+	printf("\n");
+}
+
+void insert(int val) {
+
+
+
+
+
+}
+
+
+
+
+
+
+
